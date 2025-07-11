2daf844a2e22d4317b1c4bf43f499edf21a71e8f
LGenSOC <149721085+LGenSOC@users.noreply.github.com>
2025-07-11 17:51:17 +0900

    Populated browserstack.config.js with required capabilities

---
+++ browserstack.config.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const commonCapabilities = {
+  "browserstack.user": process.env.BROWSERSTACK_USERNAME,
+  "browserstack.key": process.env.BROWSERSTACK_ACCESS_KEY,
+  build: "BSTACK Tech Challenge",
+  name: "Tech Challenge Test",
+  "browserstack.debug": true,
+  "browserstack.networkLogs": true,
+};
+
+const capabilities = [];
+
+// Configuration for Windows 10 Chrome
+capabilities.push({
+  ...commonCapabilities,
+  os: "Windows",
+  os_version: "10",
+  browserName: "Chrome",
+});
+
+// Configuration for macOS Ventura Firefox
+capabilities.push({
+  ...commonCapabilities,
+  os: "OS X",
+  os_version: "Ventura",
+  browserName: "Firefox",
+});
+
+// Configuration for Samsung Galaxy S22 (Real Mobile Device)
+capabilities.push({
+  ...commonCapabilities,
+  device: "Samsung Galaxy S22",
+  realMobile: "true",
+  browserName: "Android", // Use "Android" for mobile browsers on real devices
+});
+
+module.exports = {
+  capabilities,
+};
